e795d3c5183a2079e23d21e9c7c87ae2f81aaa76
Chas <Hugginsc10@gmail.com>
2022-02-13 22:55:23 -0500

    added styling to header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/Header'
 
+
 export default function Home() {
   return (
     <div className="">
@@ -8,8 +9,7 @@ export default function Home() {
         <title>Medium Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-     <Header/>
+      <Header/>
     </div>
   )
 }
